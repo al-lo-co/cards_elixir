70ee2051343e9e699051cff682b29a527923212c
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2025-06-27 08:17:31 -0500

    Initial commit, example

---
+++ cards/lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do #defining modules, collection of functions and variables
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Example of executing the function and result
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do #defining functions
+    :world #implicity return as ruby
+  end
+end
